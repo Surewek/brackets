2617a280970e5a9727f0e8ac60878ae7f7ff24cb
Vitaly Vegera <surewek@gmail.com>
2022-03-11 22:06:25 +0300

    feat: complite brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let bracketsPair = {};
+  let openBrackets = [];
+
+  bracketsConfig.forEach((element) => {
+    openBrackets.push(element[0]);
+    bracketsPair[element[1]] = element[0];
+  });
+
+  for(let i = 0; i < str.length; i++){
+    let currentSymbol = str[i];
+    
+    if((openBrackets.includes(currentSymbol) && bracketsPair[currentSymbol] !== currentSymbol) ||
+     (bracketsPair[currentSymbol] === currentSymbol && stack[stack.length - 1] !== currentSymbol)){
+      stack.push(currentSymbol);
+    } else {
+      if(stack.length === 0){
+        return false;
+      }
+
+      let lastSymbol = stack[stack.length - 1];
+
+      if(bracketsPair[currentSymbol] === lastSymbol){
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  if(stack.length === 0){
+    return true;
+  } else {
+    return false;
+  }
 }
